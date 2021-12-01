ab9c34651fd90c1fa099cfe940b3dc2f5ff82ff6
Youssef Restom <youssefrestom@gmail.com>
2021-12-01 13:15:42 +0100

    feat: set rebate account and cost center filters

--- rebatems/rebate_management/doctype/rebate_policy/rebate_policy.js
+++ rebatems/rebate_management/doctype/rebate_policy/rebate_policy.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Rebate Policy', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		frm.set_query('cost_center', () => {
+			return {
+				filters: {
+					company: frm.doc.company
+				}
+			};
+		});
+		frm.set_query('rebate_account', () => {
+			return {
+				filters: {
+					company: frm.doc.company,
+					account_type: ['in', ["Tax", "Chargeable", "Income Account", "Expenses Included In Valuation", "Expenses Included In Asset Valuation"]],
+				}
+			};
+		});
+	}
 });
